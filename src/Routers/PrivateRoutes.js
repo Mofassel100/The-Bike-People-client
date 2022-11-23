542924187a93871c46502714c5fec846caaf124a
Mofassel100 <smmofasselhosain.bba.ac.22@gmail.com>
2022-11-24 00:24:35 +0600

    private router added

--- src/Routers/PrivateRoutes.js
+++ src/Routers/PrivateRoutes.js
@@ -1,10 +1,27 @@
+import { useContext } from "react";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
 
 
 
 const PrivateRouter =({children}) => {
   
+    const {user,loader}=useContext(AuthContext)
+    const location =useLocation()
+   
+     if(loader){
+        return <h3 className='text-3xl'>Loading</h3>
+     }
+     if(!user){
+        return <Navigate to='/login' state={{from:location}} replace />
+
+
+     }
+     return children
      
     
-};
+}; 
+    
+
 
 export default PrivateRouter;
